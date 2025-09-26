eb1e1213d0af4dbd83093e3741da45fdf8a85ce0
Kwabena <kwabena.a92@gmail.com>
2025-09-27 01:50:36 +0200

    Add option for STARTTLS

--- cms/src/payload.config.ts
+++ cms/src/payload.config.ts
@@ -33,7 +33,7 @@ export default buildConfig({
           transportOptions: {
             host: process.env.SMTP_HOST,
             port: Number(process.env.SMTP_PORT),
-            secure: true,
+            secure: process.env.SMTP_SECURE === "true",
             auth: {
               user: process.env.SMTP_USER,
               pass: process.env.SMTP_PASS,
