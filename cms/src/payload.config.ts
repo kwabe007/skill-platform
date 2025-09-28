541259fdc984d06ff9401feee6886fe5867371fc
Kwabena <kwabena.a92@gmail.com>
2025-09-28 22:37:32 +0200

    Change default depth to 1

--- cms/src/payload.config.ts
+++ cms/src/payload.config.ts
@@ -24,6 +24,7 @@ export default buildConfig({
   },
   collections: [Users, Media, Skills],
   editor: lexicalEditor(),
+  defaultDepth: 1,
   email:
     process.env.DISABLE_EMAIL !== "true"
       ? nodemailerAdapter({
