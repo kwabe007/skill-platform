bbf9e0f4845d1e8987d2756bb3efc45fca345d7a
Kwabena <kwabena.a92@gmail.com>
2025-10-05 17:46:31 +0200

    Add importMap.js to .prettierignore

--- cms/src/app/(payload)/admin/importMap.js
+++ cms/src/app/(payload)/admin/importMap.js
@@ -1,5 +1,5 @@
-import { default as default_ad212af134249a244a222238080de703 } from "../../../components/EmailSettingsUI";
+import { default as default_ad212af134249a244a222238080de703 } from '../../../components/EmailSettingsUI'
 
 export const importMap = {
-  "/components/EmailSettingsUI#default": default_ad212af134249a244a222238080de703,
-};
+  "/components/EmailSettingsUI#default": default_ad212af134249a244a222238080de703
+}
